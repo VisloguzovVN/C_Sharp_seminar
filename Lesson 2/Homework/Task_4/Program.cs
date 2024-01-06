9eaecd02c3cf63f70a18069cb358a099c9b10f08
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-01-06 21:41:56 +0800

    add lesson_2:Homework_Task4_final

--- Lesson 2/Homework/Task_4/Program.cs
+++ Lesson 2/Homework/Task_4/Program.cs
@@ -3,21 +3,20 @@ int N = Convert.ToInt32(Console.ReadLine());
 int result = 0;
 while (N > 0)
 {
-    result = (result*10 + N % 10) - 10;
+    result = result*10 + N % 10;
     N /= 10;
 }
-Console.Write(result);
-if (N < 10)
+if (result < 10)
 {
 Console.WriteLine(N);
 }
 else
 {
-    while (N > 0)
+    while (result > 0)
     {
-        int Digit = N % 10;
-        N /= 10;
-        if (N > 0)
+        int Digit = result % 10;
+        result /= 10;
+        if (result > 0)
         {
             Console.Write(Digit + ",");
         }
