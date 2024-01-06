d144ed249efbed011e337931c189eb20d8e90498
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-01-06 16:39:26 +0800

    add lesson_2:Homework_Task3

---
+++ Lesson 2/Homework/Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write number from the segment [10, 99]  ");
+int X = int.Parse(Console.ReadLine()!);
+int a = X / 10;
+int b = X % 10;
+if(a >= b)
+{
+    Console.WriteLine(a);
+}
+else 
+{
+    Console.WriteLine(b);
+}
