5657a6ea6188756866afd2eeaf00317bd13b2202
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-01-06 20:17:44 +0800

    add lesson_2:Homework_Task4

---
+++ Lesson 2/Homework/Task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+Console.Write("Write natural number N:  ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 10)
+{
+Console.WriteLine(N);
+}
+else
+{
+    while (N > 0)
+    {
+        int Digit = N % 10;
+        N /= 10;
+        if (N > 0)
+        {
+            Console.Write(Digit + ",");
+        }
+        else
+        {
+            Console.WriteLine(Digit);
+        }
+    }
+}
