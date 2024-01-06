34c8d77ff9253bb1ea7bb586a997a38aa2450a28
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-01-06 16:03:34 +0800

    add lesson_2:Homework_Task2

---
+++ Lesson 2/Homework/Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Write X  ");
+Console.Write("Write Y  ");
+int X = int.Parse(Console.ReadLine()!);
+int Y = int.Parse(Console.ReadLine()!);
+if(X > 0 & Y > 0)
+{
+   Console.WriteLine("First sector"); 
+}
+else if(X < 0 & Y > 0)
+{
+    Console.WriteLine("Second sector");
+}
+else if(X < 0  & Y < 0)
+{
+    Console.WriteLine("Third sector");
+}
+else if(X > 0 & Y < 0)
+{
+    Console.WriteLine("Fourth sector");
+}
+else
+{
+    Console.WriteLine("A point on the coordinate axis");
+}
