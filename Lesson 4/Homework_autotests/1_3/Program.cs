e51d226eb7b87f23e5f921be23ba33c335ab9534
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-01-16 20:16:37 +0800

    add Homework_3

---
+++ Lesson 4/Homework_autotests/1_3/Program.cs
@@ -0,0 +1,67 @@
+﻿//Задайте массив из вещественных чисел с ненулевой дробной частью. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    // Разница между максимальным и минимальным элементами массива
+    
+    // Нахождение минимума массива
+    public static double FindMin(double[] numbers)
+    {
+        double minVal= 0.0;
+            foreach(double item in numbers) 
+            {
+                if(item < minVal) 
+                minVal = item;
+            }
+            return minVal;
+    }
+    
+    // Нахождение максимума массива
+    public static double FindMax(double[] numbers)
+    {
+        double maxVal = 0.0;
+        foreach(double item in numbers)
+        {
+            if(item < maxVal)
+            maxVal = item;
+        }
+        return maxVal;
+    }
+    
+    public static void PrintResult(double[] array)
+    {
+      Console.WriteLine(FindMax(array) - FindMin(array));
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        double[] array;
+        
+
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(double.Parse)
+                                  .ToArray();
+            
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+        } else {
+           // Если аргументов на входе нет
+            array = new double[] {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
