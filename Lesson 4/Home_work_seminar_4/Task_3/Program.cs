3a31cbe57b73c94fff1f713968c4f012919bf0b0
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-01-18 15:45:16 +0800

    add homework Lesson4: Task3

--- Lesson 4/Home_work_seminar_4/Task_3/Program.cs
+++ Lesson 4/Home_work_seminar_4/Task_3/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = {7, 1, 3, 2, 6, 4, 8, 5, 9}; 
+        int temp;
+        
+        Console.Write("Одномерный массив для реверса: ");
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+        for (int i = 0; i < numbers.Length / 2; i++)
+        {
+            temp = numbers[i];
+            numbers[i] = numbers[numbers.Length - 1 - i];
+            numbers[numbers.Length - 1 - i] = temp;
+        }
+        Console.Write("\nПеревернутый массив: ");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+         }
+    }
+}
