6de79894da4032dc1d8062e1f2552e3a7813249f
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-01-28 19:47:33 +0800

    add Lesson5:Task1, Task2, Task3

---
+++ Lesson 5/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿// 1. Задайте двумерный массив. Найдите элементы, у которых оба
+// индекса чётные, и замените эти элементы на их квадраты.
+
+// 1. Задайте двумерный массив. Найдите элементы, у которых оба
+// индекса чётные, и замените эти элементы на их квадраты.
+
+int[,] CreateMatrix(int rowCount, int columsCount) // Функция создания матрицы с элементами от 1 до 999
+{
+int[,] matrix = new int[rowCount, columsCount];
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = rnd.Next(1, 11);
+}
+}
+return matrix;
+}
+
+void ShowMatrix(int[,] matrix) // Функция вывода матрицы на экран
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write($"{matrix[i, j]} ");
+}
+Console.WriteLine();
+}
+}
+
+int[,] matrix = CreateMatrix(3, 4); // Задание размера матрицы и инициализация "интересного" числа.
+ShowMatrix(matrix);
+
+void evenIndex(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i+=2)
+{
+for (int j = 0; j < array.GetLength(1); j+=2)
+{
+array[i, j] = array[i, j] * array[i, j];
+}
+}
+}
+evenIndex(matrix);
+ShowMatrix(matrix);
