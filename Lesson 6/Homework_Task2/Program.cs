7770b4b1437cd168f2c933b65e444990c329d53a
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-02-03 12:27:15 +0800

    add some change in  Homework_Task2

--- Lesson 6/Homework_Task2/Program.cs
+++ Lesson 6/Homework_Task2/Program.cs
@@ -1,14 +1,11 @@
 ﻿//Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
 
-public class Task2
+public class HomeworkTask02
 {
     public static void Main(string[] args)
     {
-        // Входная строка со смешанными буквами обоих регистров
         string input = "sKlUrTG2JhGBvD!-";
-        // Преобразование всех заглавных букв в строчные
         string result = input.ToLower();
-        // Вывод результата
         Console.WriteLine(result);
     }
 }
