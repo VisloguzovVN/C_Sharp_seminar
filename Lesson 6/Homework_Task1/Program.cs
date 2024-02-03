7d51c46d2463ce81f9a4d9919d1a8e0c2f06aa90
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-02-03 11:54:16 +0800

    add Homework_Task1

---
+++ Lesson 6/Homework_Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+public class HomeworkTask01
+{
+    public static void Main(string[] args)
+    {
+        char[,] charArray = new char[,] { { 'k', 'f' }, { 's', 'k' } };
+        string result = CreateStringFromArray(charArray);
+        Console.WriteLine(result);
+    }
+        public static string CreateStringFromArray(char[,] array)
+        {
+            string result = "";
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        result += array[i, j];
+                    }
+                }
+                return result;
+        }
+}
