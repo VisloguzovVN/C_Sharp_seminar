43c017d3c3b142a0e17c748f0c6dd00ba8abe12b
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-02-03 12:15:01 +0800

    add Homework6_Task3

---
+++ Lesson 6/Homework_Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+public class HomeworkTask03
+{
+public static void Main(string[] args)
+    {
+    string input = "рвал дед лавр";
+    bool isPalindrome = IsPalindrome(input);
+    Console.WriteLine(isPalindrome ? "Палиндром" : "Не палиндром");
+    }
+    public static bool IsPalindrome(string str)
+    {
+        string normalized = new
+        string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+    return normalized.SequenceEqual(normalized.Reverse());
+    }
+}
