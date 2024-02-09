65e20965d1457059cfbb3a85e2a55ea204f531a0
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-02-09 21:48:30 +0800

    add homework task 1

---
+++ Lesson 7/Homework/Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number M:");
+        int M = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Enter number N:");
+        int N = int.Parse(Console.ReadLine()!);
+
+        PrintNumbers(M, N);
+
+        Console.ReadLine();
+    }
+
+    static void PrintNumbers(int start, int end)
+    {
+        if (start > end)
+        {
+            return;
+        }
+
+        Console.WriteLine(start);
+        PrintNumbers(start + 1, end);
+    }
+}
