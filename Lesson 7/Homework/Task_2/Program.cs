2b19771bd49c08902f9d9f363047322bcd209f77
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-02-09 22:17:28 +0800

    add homework task2.2

---
+++ Lesson 7/Homework/Task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 2.2: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N
+// с помощью рекурсии.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter value of the number M:");
+        int M = int.Parse(Console.ReadLine()!);
+        
+        Console.WriteLine("Enter value of the number N:");
+        int N = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Even numbers from {0} to {1}: ", M, N);
+        PrintEvenNumbers(M, N);
+
+        Console.WriteLine();
+    }
+    
+    static void PrintEvenNumbers(int currentNumber, int N)
+    {
+        if(currentNumber > N)
+            return;
+        
+        if(currentNumber % 2 == 0)
+            Console.WriteLine(currentNumber);
+        
+        PrintEvenNumbers(currentNumber + 1, N);
+    }
+}
+
